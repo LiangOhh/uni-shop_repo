1d41e02bf3d06b721951b6640829f62dc118028b
Liang <Liang@163.com>
2024-01-25 16:27:48 +0800

    购物车渲染2

---
+++ components/MySettle/MySettle.vue
@@ -0,0 +1,78 @@
+<template>
+	<view>
+		<view class="settle">
+			<label class="radio" @click="changeAllState">
+				<radio color="#C00000" :checked="isFullCheck" /><text>全选</text>
+			</label>
+			<view class="amount-box">
+				合计:<text class="amount">￥{{checkedGoodsAmount}}</text>
+			</view>
+			<view class="btn-settle">结算{{checkedCount}}</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	import {
+		mapGetters,
+		mapMutations
+	} from 'vuex'
+	export default {
+		name: "MySettle",
+		data() {
+			return {
+
+			};
+		},
+		computed: {
+			...mapGetters('m_cart', ['checkedCount', 'getTotal', 'checkedGoodsAmount']),
+			isFullCheck() {
+				return this.getTotal === this.checkedCount
+			},
+		},
+		methods: {
+			...mapMutations('m_cart', ['updateAllGoodsState']),
+			changeAllState() {
+				// 修改购物车中所有商品的选中状态
+				// !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
+				this.updateAllGoodsState(!this.isFullCheck)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.settle {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 50px;
+		background-color: white;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding-left: 5px;
+		font-size: 14px;
+	}
+
+	.radio {
+		display: flex;
+		align-items: center;
+	}
+
+	.amount {
+		color: #c00000;
+	}
+
+	.btn-settle {
+		height: 50px;
+		min-width: 100px;
+		background-color: #c00000;
+		color: white;
+		line-height: 50px;
+		text-align: center;
+		// padding: 0 10px;
+	}
+</style>
